8953acd94520ac20c1d9966469b1d4b2bff881e4
Your Name <you@example.com>
2025-04-14 01:25:32 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let name = "Hubert" //String
+let age = 25 // Number
+let dis = "Dhaka" // String
+let allDet = "My name is" + " " + name + " " + "and my age is" + " " + age + " " + "and I live in" + " " + dis
+console.log(allDet) // Shows the output of "allDet"
+
+console.log(typeof(name))
+console.log(typeof(age))
+console.log(typeof(allDet))
+//TypeOf is a keyword that identify the data type of any variable.
+
+let num = 5 //Here is the multiplication where we can change any of the number
+
+console.log(num + " " + "X" + " " + 1 + " " + "=" + " " + (num*1))
+console.log(num + " " + "X" + " " + 2 + " " + "=" + " " + (num*2))
+console.log(num + " " + "X" + " " + 3 + " " + "=" + " " + (num*3))
+console.log(num + " " + "X" + " " + 4 + " " + "=" + " " + (num*4))
+console.log(num + " " + "X" + " " + 5 + " " + "=" + " " + (num*5))
+console.log(num + " " + "X" + " " + 6 + " " + "=" + " " + (num*6))
+console.log(num + " " + "X" + " " + 7 + " " + "=" + " " + (num*7))
+console.log(num + " " + "X" + " " + 8 + " " + "=" + " " + (num*8))
+console.log(num + " " + "X" + " " + 9 + " " + "=" + " " + (num*9))
+console.log(num + " " + "X" + " " + 10 + " " + "=" + " " + (num*10))
